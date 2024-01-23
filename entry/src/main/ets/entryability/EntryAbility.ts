aa9b5c5a020992ecdd7187dc040ca5e507a0b04b
daiweibao <497255156@qq.com>
2024-01-23 20:04:11 +0800

    学习

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,10 +1,12 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
+import promptAction from '@ohos.promptAction';
 
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+    console.log('成功：'+want)
   }
 
   onDestroy() {
